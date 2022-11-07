f86e0515e795c98d1e73fc063e6a6062dcb1b569
Eric Nantz <thercast@gmail.com>
2022-11-07 11:22:48 -0500

    add deployment app.R and minor cleanup

--- R/app_server.R
+++ R/app_server.R
@@ -13,46 +13,27 @@ app_server <- function(input, output, session, random_question_order = TRUE) {
   start_time <- reactiveVal(Sys.time())
   auth_method <- reactiveVal()
   
-  #f <- FirebaseSocial$new()
-  if (golem::app_prod()) {
-    f <- firebase::FirebaseUI$
-      new()$ # instantiate
-      set_providers( # define providers
-        email = TRUE,
-        google = TRUE,
-        github = TRUE
-      )
-  }
-  
   if (golem::app_prod()) {
     showModal(
       modalDialog(
         title = "Login",
         firebase::firebaseUIContainer(),
         size = "xl",
-        # actionButton("google", "Google", icon = icon("google"), class = "btn-danger"),
-        # actionButton("github", "GitHub", icon = icon("github")),
-        # actionButton("email", "Email"),
         footer = NULL
       )
     )
-    f$launch()
   }
   
-  # observeEvent(input$google, {
-  #   auth_method("google")
-  #   f$launch_google()
-  # })
-  # 
-  # observeEvent(input$github, {
-  #   auth_method("github")
-  #   f$launch_github()
-  # })
-  # 
-  # observe({
-  #   f$req_sign_in()
-  #   removeModal()
-  # })
+  if (golem::app_prod()) {
+    f <- firebase::FirebaseUI$
+      new()$ # instantiate
+      set_providers( # define providers
+        google = TRUE,
+        github = TRUE,
+        microsoft = TRUE
+      )$
+      launch()
+  }
   
   if (golem::app_prod()) observeEvent(f$req_sign_in(), removeModal(session))
   
@@ -62,14 +43,7 @@ app_server <- function(input, output, session, random_question_order = TRUE) {
       f$req_sign_in()
     }
     start_app(runif(1))
-    
-    # if (golem::app_prod()) {
-    #   req(auth_method())
-    #   browser()
-    #   f$req_sign_in()
-    # }
     ui_secret()
-    #start_app(runif(1))
   })
   
   # load quiz data
@@ -82,6 +56,7 @@ app_server <- function(input, output, session, random_question_order = TRUE) {
   }
   
   observeEvent(start_app(), {
+    browser()
     showModal(
       modalDialog(
         title = "Welcome",
